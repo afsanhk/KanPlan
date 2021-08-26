591108e03f6195c86278d8e9ce840d9ea492e942
Afsanul Haque Khan <afsanhk@gmail.com>
2021-08-26 19:45:01 +0000

    Add morgan, bodyParser and dotenv to /server

--- server/index.js
+++ server/index.js
@@ -1,7 +1,14 @@
 // This will contain all server code.
+// load .env data into process.env
+require("dotenv").config();
+
 const express = require("express");
 const PORT = process.env.PORT || 5000;
 
 const app = express();
+const bodyParser = require("body-parser");
+const morgan = require("morgan");
 
 app.listen(PORT, () => console.log(`Server has started on port ${PORT}`));
+
+// need to install dot-env
