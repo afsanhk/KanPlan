bb2b2d7edb97cdd0f17aa827db2f88f277ec8c80
Afsanul Haque Khan <afsanhk@gmail.com>
2021-08-27 18:00:26 -0400

    api/projects/ returns an array of user ids as team_members

--- server/src/routes/projects.js
+++ server/src/routes/projects.js
@@ -3,9 +3,12 @@ const router = require("express").Router();
 module.exports = (db) => {
   router.get("/projects", (request, response) => {
     db.query(
-      `SELECT projects.*,users.user_name as manager_name 
+      `SELECT projects.*,users.user_name as manager_name, array_agg(DISTINCT project_members.user_id) AS team_members 
       FROM projects 
-      JOIN users ON manager_id = users.id`
+      JOIN users ON projects.manager_id = users.id
+      JOIN project_members ON project_members.project_id = projects.id
+      GROUP BY projects.id, users.user_name
+      ORDER BY projects.id`
     ).then(({ rows: projects }) => {
       response.json(projects);
     });
