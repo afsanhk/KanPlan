b2c56f55279539598e8157f56b2f2ba67dfaa792
Taejin Jung <taejin5314@gmail.com>
2021-08-27 19:11:20 -0400

    install material ui labs and add html for EditTaskForm component

--- client/src/components/EditTaskForm.jsx
+++ client/src/components/EditTaskForm.jsx
@@ -1,6 +1,16 @@
 import React from 'react';
 import TeamMember from './TeamMember';
 
+import TextField from '@material-ui/core/TextField';
+import Autocomplete from '@material-ui/lab/Autocomplete';
+
+const projectNames = [
+  { title: 'The Shawshank Redemption', year: 1994 },
+  { title: 'The Godfather', year: 1972 },
+  { title: 'The Godfather: Part II', year: 1974 },
+  { title: 'The Dark Knight', year: 2008 }
+];
+
 function EditTaskForm(props) {
   return (
     <div>
@@ -16,10 +26,42 @@ function EditTaskForm(props) {
           </p>
         </div>
 
-        <div className="task-form-body-dropdowns"></div>
+        <div className="task-form-body-dropdowns">
+          <div className="task-form-body-dropdowns-project">
+            <Autocomplete
+              id="combo-box-demo"
+              options={projectNames}
+              getOptionLabel={(option) => option.title}
+              style={{ width: '80%' }}
+              renderInput={(params) => <TextField {...params} label="Project Title" variant="outlined" />}
+            />
+          </div>
+
+          <div className="task-form-body-dropdowns-date"></div>
+
+          <div className="task-form-body-dropdowns-status">
+            <Autocomplete
+              id="combo-box-demo"
+              options={projectNames}
+              getOptionLabel={(option) => option.title}
+              style={{ width: 300 }}
+              renderInput={(params) => <TextField {...params} label="Status" variant="outlined" />}
+            />
+            <Autocomplete
+              id="combo-box-demo"
+              options={projectNames}
+              getOptionLabel={(option) => option.title}
+              style={{ width: 300 }}
+              renderInput={(params) => <TextField {...params} label="Priority" variant="outlined" />}
+            />
+          </div>
+        </div>
 
         <div className="task-form-body-members">
-          <TeamMember remove />
+          <TeamMember name="Afsan" remove />
+          <TeamMember name="Person X" remove />
+          <TeamMember name="TJ" remove />
+          <TeamMember name="Veronica" remove />
         </div>
       </div>
 
