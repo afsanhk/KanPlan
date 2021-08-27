71fa9c9a671a5e895690b3d51f3da6953247066d
Veronica Leung <tungtungleung233@hotmail.com>
2021-08-27 14:36:19 -0400

    add Material UI and TaskList component

---
+++ client/src/components/TaskListItem.jsx
@@ -0,0 +1,50 @@
+import React from "react";
+
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+
+import { makeStyles } from '@material-ui/core/styles'; //use this to customize the style
+
+//icons
+import IconButton from '@material-ui/core/IconButton'; 
+import EditOutlinedIcon from '@material-ui/icons/EditOutlined';
+import DeleteOutlinedIcon from '@material-ui/icons/DeleteOutlined';
+
+
+const useStyles = makeStyles({
+  listItem: {
+    '&:hover': {
+      backgroundColor: '#f5f5f5',
+    }
+  }
+}) 
+
+export default function TaskListItem(props) {
+
+  const classes = useStyles();
+
+  return (
+    <ListItem className={classes.listItem}>
+        <ListItemText primary={props.title} />
+
+        <ListItemIcon>
+
+          {/* onClick will trigger the edit modal */}
+          <IconButton >
+            <EditOutlinedIcon />
+          </IconButton>
+
+          {/* onClick will trigger the delete modal */}
+          <IconButton >
+            <DeleteOutlinedIcon />
+          </IconButton>
+
+        </ListItemIcon>
+
+    </ListItem>
+
+  )
+}
+
+
