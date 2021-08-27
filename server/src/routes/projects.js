8e528593bf2dfcaca4d1b4ae85a220d37e2d0758
Afsanul Haque Khan <afsanhk@gmail.com>
2021-08-27 19:09:09 -0400

    Comment out monster query with json_build_obj and change back to basic query that only returns ids

--- server/src/routes/projects.js
+++ server/src/routes/projects.js
@@ -10,7 +10,7 @@ module.exports = (db) => {
       FROM projects 
       JOIN users ON projects.manager_id = users.id
       JOIN project_members ON project_members.project_id = projects.id
-      JOIN tasks ON tasks.project_id = projects.id
+      LEFT JOIN tasks ON tasks.project_id = projects.id
       GROUP BY projects.id, users.user_name
       ORDER BY projects.id`
     ).then(({ rows: projects }) => {
@@ -20,3 +20,17 @@ module.exports = (db) => {
 
   return router;
 };
+//
+
+// // Buggy query with 33 tasks and only one project
+// `SELECT projects.*,
+//               users.user_name as manager_name,
+//               array_agg(DISTINCT project_members.user_id) AS team_members,
+//               array_agg(DISTINCT tasks.id) AS project_tasks,
+//               json_agg(json_build_object('task_id', tasks.id, 'task_title', tasks.title, 'task_description', tasks.task_description, 'priority_id', tasks.priority_id, 'status_id', tasks.status_id, 'plan_start', tasks.plan_start, 'plan_end', tasks.plan_end)) AS project_task
+//       FROM projects
+//       JOIN users ON projects.manager_id = users.id
+//       JOIN project_members ON project_members.project_id = projects.id
+//       LEFT JOIN tasks ON tasks.project_id = projects.id
+//       GROUP BY projects.id, users.user_name
+//       ORDER BY projects.id`
