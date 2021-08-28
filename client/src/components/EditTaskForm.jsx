63323cc42b717f5cfdef098c6bcc5cb7bba88526
Taejin Jung <taejin5314@gmail.com>
2021-08-28 17:39:36 -0400

    add IconButton

--- client/src/components/EditTaskForm.jsx
+++ client/src/components/EditTaskForm.jsx
@@ -142,7 +142,9 @@ function EditTaskForm({ tasks, userProjects, taskStatus, taskPriority }) {
             <div className="task-form-body-members-title">
               <div>
                 <h2>Team Members</h2>
-                <AddCircleIcon className={classes.teamMemberButton} fontSize="large" />
+                <IconButton size="medium">
+                  <AddCircleIcon className={classes.teamMemberButton} fontSize="large" />
+                </IconButton>
               </div>
             </div>
 
