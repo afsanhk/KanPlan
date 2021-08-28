3d77e343693b0701c09d2c47619ad53e11e5f746
Afsanul Haque Khan <afsanhk@gmail.com>
2021-08-28 15:32:48 -0400

    Add user_tasks array to api/users endpoint

--- server/src/routes/users.js
+++ server/src/routes/users.js
@@ -1,8 +1,15 @@
-const router = require('express').Router();
+const router = require("express").Router();
 
 module.exports = (db) => {
-  router.get('/users', (request, response) => {
-    db.query(`SELECT * FROM users`).then(({ rows: users }) => {
+  router.get("/users", (request, response) => {
+    db.query(
+      `SELECT users.*, 
+              array_agg(DISTINCT tasks.id) AS user_tasks 
+      FROM users
+      JOIN user_tasks ON user_tasks.user_id = users.id 
+      JOIN tasks ON user_tasks.task_id = tasks.id
+      GROUP BY users.id`
+    ).then(({ rows: users }) => {
       response.json(users);
     });
   });
