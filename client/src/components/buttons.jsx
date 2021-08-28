6754fd50d7814cab47f06475eeefb24ff4b966df
Taejin Jung <taejin5314@gmail.com>
2021-08-28 15:08:12 -0400

    adding button component

---
+++ client/src/components/buttons.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    '& > *': {
+      margin: theme.spacing(1),
+    },
+  },
+}));
+
+function buttons() {
+  return (
+    <div>
+      <Button
+    </div>
+  )
+}
+
+export default buttons
