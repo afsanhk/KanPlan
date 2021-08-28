e11c896b6a52e394595797887099454bd8f65374
Veronica Leung <tungtungleung233@hotmail.com>
2021-08-28 18:33:26 -0400

    started navbar component

---
+++ client/src/components/NavBar.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+
+import Drawer from '@material-ui/core/Drawer';
+import List from '@material-ui/core/List';
+import List from '@material-ui/core/List';
+
+function NavBar() {
+  return (
+    <Drawer
+        className={classes.drawer}
+        variant="permanent"
+        classes={{
+          paper: classes.drawerPaper,
+        }}
+        anchor="left"
+      >
+        <div className={classes.toolbar} />
+        <Divider />
+        <List>
+          {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
+            <ListItem button key={text}>
+              <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItem>
+          ))}
+        </List>
+        <Divider />
+        <List>
+          {['All mail', 'Trash', 'Spam'].map((text, index) => (
+            <ListItem button key={text}>
+              <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItem>
+          ))}
+        </List>
+      </Drawer>
+  )
+}
+
+export default NavBar
