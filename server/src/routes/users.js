125a3bcc07d1dc720573d0a9ac401661b36cfbd3
Afsanul Haque Khan <afsanhk@gmail.com>
2021-08-28 16:32:50 -0400

    Add array of user_projects to the /api/users endpoint

--- server/src/routes/users.js
+++ server/src/routes/users.js
@@ -4,10 +4,11 @@ module.exports = (db) => {
   router.get("/users", (request, response) => {
     db.query(
       `SELECT users.*, 
-              array_agg(DISTINCT tasks.id) AS user_tasks
+              array_agg(DISTINCT user_tasks.task_id) AS user_tasks,
+              array_agg(DISTINCT project_members.project_id) AS user_projects
       FROM users
       LEFT JOIN user_tasks ON user_tasks.user_id = users.id 
-      LEFT JOIN tasks ON user_tasks.task_id = tasks.id
+      JOIN project_members ON project_members.user_id = users.id
       GROUP BY users.id`
     ).then(({ rows: users }) => {
       response.json(users);
