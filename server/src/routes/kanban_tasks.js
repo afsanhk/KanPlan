16b377eb90aa1f4f7fae35cccd5f1a064e5007a2
Afsanul Haque Khan <afsanhk@gmail.com>
2021-08-28 15:38:30 -0400

    Add kanban_tasks.js route which is called as kanbanTasks function in application.js in the server directory.

---
+++ server/src/routes/kanban_tasks.js
@@ -0,0 +1,21 @@
+const router = require("express").Router();
+
+module.exports = (db) => {
+  router.get("/kanbantasks", (request, response) => {
+    db.query(
+      `SELECT tasks.*, 
+              proj_name, 
+              priority_name, 
+              status 
+      FROM tasks 
+      JOIN priorities ON tasks.priority_id = priorities.id
+      JOIN projects ON tasks.project_id = projects.id
+      JOIN kanban_status ON tasks.status_id = kanban_status.id
+      ORDER BY tasks.id`
+    ).then(({ rows: tasks }) => {
+      response.json(tasks);
+    });
+  });
+
+  return router;
+};
