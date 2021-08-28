a8edb21071270ea537b4a500e0e015701aeb6b8f
Afsanul Haque Khan <afsanhk@gmail.com>
2021-08-28 15:35:58 -0400

    api/users endpoint is now showing all users even if they don't have tasks

--- server/src/routes/users.js
+++ server/src/routes/users.js
@@ -6,8 +6,8 @@ module.exports = (db) => {
       `SELECT users.*, 
               array_agg(DISTINCT tasks.id) AS user_tasks 
       FROM users
-      JOIN user_tasks ON user_tasks.user_id = users.id 
-      JOIN tasks ON user_tasks.task_id = tasks.id
+      LEFT JOIN user_tasks ON user_tasks.user_id = users.id 
+      LEFT JOIN tasks ON user_tasks.task_id = tasks.id
       GROUP BY users.id`
     ).then(({ rows: users }) => {
       response.json(users);
