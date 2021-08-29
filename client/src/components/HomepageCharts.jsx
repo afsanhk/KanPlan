f3b7e22eed7863c26bf389759e66f7c42633ea44
Veronica Leung <tungtungleung233@hotmail.com>
2021-08-29 05:03:07 -0400

    add legend to HomepageCharts component

--- client/src/components/HomepageCharts.jsx
+++ client/src/components/HomepageCharts.jsx
@@ -5,13 +5,54 @@ import HomepageChartB from './HomepageChartB'
 
 import './HomepageCharts.scss'
 
+import { createTheme, ThemeProvider } from '@material-ui/core/styles';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import StopRoundedIcon from '@material-ui/icons/StopRounded';
+
+const theme = createTheme({
+  overrides: {
+    // Style sheet name ⚛️
+    MuiListItem: {
+      root: {
+        'padding-top': 0,
+        'padding-bottom': 0,
+      } 
+    },
+  },
+});
+
+
 function HomepageCharts() {
   return (
-    <div className='homepage-charts'>
-      <HomepageChartA chartInformation={5} chartTitle='Projects Managing:' chartColor='#0099ff' />
-      <HomepageChartA chartInformation={1} chartTitle='Projects Working On:' chartColor='#ff6699' />
-      <HomepageChartB chartInformation={[1,2,3,4]} />
-    </div>
+    <ThemeProvider theme={theme}>
+      <div className='homepage-charts'>
+        <HomepageChartA chartInformation={5} chartTitle='Projects Managing:' chartColor='#0099ff' />
+        <HomepageChartA chartInformation={1} chartTitle='Projects Working On:' chartColor='#ff6699' />
+        <div className='homepage-chart-B-legend'>
+          <HomepageChartB chartInformation={[1,2,3,4]} />
+          <List>
+            <ListItem>
+              <StopRoundedIcon className='homepage-chart-legend-late' />
+              <ListItemText>Late</ListItemText>
+            </ListItem>
+            <ListItem>
+              <StopRoundedIcon className='homepage-chart-legend-to-do' />
+              <ListItemText>To-Do</ListItemText>
+            </ListItem>
+            <ListItem>
+              <StopRoundedIcon className='homepage-chart-legend-in-progress' />
+              <ListItemText>In Progress</ListItemText>
+            </ListItem>
+            <ListItem>
+              <StopRoundedIcon className='homepage-chart-legend-done' />
+              <ListItemText>Done</ListItemText>
+            </ListItem>
+          </List>
+        </div>
+      </div>
+    </ThemeProvider>
   )
 }
 
