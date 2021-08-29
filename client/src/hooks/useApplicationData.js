7a74c2d8a8c0ec1a67000ac962402d6b638471be
Afsanul Haque Khan <afsanhk@gmail.com>
2021-08-29 17:40:57 -0400

    Small changes - initialize state with {} instead of [] in useApplicationData.js. Remove console.log(state) from App.js

--- client/src/hooks/useApplicationData.js
+++ client/src/hooks/useApplicationData.js
@@ -2,12 +2,11 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 
 export default function useApplicationData() {
-
-  const [state, setState] = useState({  
-    tasks: [],
-    projects: [],
-    users: [],
-  })
+  const [state, setState] = useState({
+    tasks: {},
+    projects: {},
+    users: {},
+  });
 
   useEffect(() => {
     Promise.all([
@@ -15,16 +14,15 @@ export default function useApplicationData() {
       axios.get("http://localhost:8001/api/projects"),
       axios.get("http://localhost:8001/api/users"),
     ]).then((all) => {
-      //updates the satte with all the information received from the axios get requests
-      setState(prev => ({
+      //updates the state with all the information received from the axios get requests
+      setState((prev) => ({
         ...prev,
         tasks: all[0].data,
         projects: all[1].data,
         users: all[2].data,
-      }))
-    })
+      }));
+    });
   }, []); //empty square brackets ensures that this useEffect is only ran once during page load
 
-
-  return {state};
+  return { state };
 }
