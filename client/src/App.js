a3b5d51875c182d19c4a8e43fe152c572a1d5c9a
Afsanul Haque Khan <afsanhk@gmail.com>
2021-08-29 08:53:10 -0400

    Add switch routes for react-router. These will need to be updated as implementation proceeds.

--- client/src/App.js
+++ client/src/App.js
@@ -2,8 +2,13 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
-// Components
+// Components & Pages
 import NavBar from "./components/NavBar";
+import UserDashboard from "./pages/UserDashboard";
+import UserProjects from "./pages/UserProjects";
+import ProjectOverview from "./pages/ProjectOverview";
+import ProjectKanban from "./pages/ProjectKanban";
+import ProjectGantt from "./pages/ProjectGantt";
 
 // Styling
 import "./App.scss";
@@ -11,9 +16,29 @@ import "./App.scss";
 function App() {
   return (
     <div className="App">
-      <h1>KanPlan!</h1>
       <Router>
+        <h1>KanPlan!</h1>
         <NavBar />
+        <Switch>
+          <Route exact path="/">
+            <UserDashboard />
+          </Route>
+          <Route path="/projects">
+            <UserProjects />
+          </Route>
+          {/* This will need to change to become a dynamic link. */}
+          <Route path="/project/overview">
+            <ProjectOverview />
+          </Route>
+          {/* This will need to change to become a dynamic link. */}
+          <Route path="/project/kanban">
+            <ProjectKanban />
+          </Route>
+          {/* This will need to change to become a dynamic link. */}
+          <Route path="/project/gantt">
+            <ProjectGantt />
+          </Route>
+        </Switch>
       </Router>
     </div>
   );
