13ec12abf5903eeba6f1cef1160de3b4347ebc73
Afsanul Haque Khan <afsanhk@gmail.com>
2021-08-29 11:04:47 -0400

    Add in-line margin to ganttContainer so that it does not overlap with navbar. Add future task to check scrolling

--- client/src/pages/projectGantt.js
+++ client/src/pages/projectGantt.js
@@ -43,13 +43,27 @@ const projectTasks = [
     status: "To-Do",
     task_users: [1],
   },
+  {
+    id: 4,
+    title: "Future task",
+    task_description: "Build Kanban containers and drag and drop",
+    priority_id: 2,
+    status_id: 1,
+    project_id: 1,
+    plan_start: "2022-12-29T18:08:57.766Z",
+    plan_end: "2022-12-31T18:08:57.766Z",
+    proj_name: "KanPlan",
+    priority_name: "Low",
+    status: "To-Do",
+    task_users: [1],
+  },
 ];
 
 const ProjectGantt = () => {
   return (
     <div>
       <h1>This will show the project Gantt.</h1>
-      <section class="ganttContainer">
+      <section className="ganttContainer" style={{ marginLeft: "140px" }}>
         <Gantt projectTasks={projectTasks} />
       </section>
     </div>
