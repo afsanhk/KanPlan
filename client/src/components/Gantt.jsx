392473c9cdfefb261f58cfbb29de7fe54c88cf84
Afsanul Haque Khan <afsanhk@gmail.com>
2021-08-29 18:26:30 -0400

    Remove big commented test data from Gantt.jsx

--- client/src/components/Gantt.jsx
+++ client/src/components/Gantt.jsx
@@ -10,41 +10,6 @@ Since our task data is much more robust:
 We do 2 maps, first one is to convert our structure into the required structure. Second is to convert that into an array of 'Task' classes as required by the library.
 */
 
-// Left for reference - this is the required structure to convert to a Task - delete when we are comfortable with how data is being worked.
-// const tasks = [
-//   {
-//     id: '1',
-//     name: 'Task 1',
-//     start: '2021-12-01',
-//     end: '2021-12-03',
-//     progress: 10,
-//     dependencies: ''
-//   },
-//   {
-//     id: '2',
-//     name: 'Task 2',
-//     start: '2021-11-16',
-//     end: '2021-11-30',
-//     progress: 20,
-//     dependencies: ''
-//   },
-//   {
-//     id: '3',
-//     name: 'Task 4',
-//     start: '2021-11-29',
-//     end: '2021-11-30',
-//     progress: 80,
-//     dependencies: ''
-//   },
-//   {
-//     id: '4',
-//     name: 'Task 5',
-//     start: '2021-11-30',
-//     end: '2021-12-17',
-//     progress: 30,
-//     dependencies: ''
-//   }
-// ].map((x) => new Task(x));
 function Gantt({projectTasks}) {
 
   const tasks = projectTasks[0] && 
