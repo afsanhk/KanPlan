01b7335249ba6bd1d39f0e53478f2fb0f32e5b91
Afsanul Haque Khan <afsanhk@gmail.com>
2021-08-29 16:04:06 -0400

    Add helpers: getTasksForProject and getProjectsForUser

--- client/src/pages/ProjectGantt.js
+++ client/src/pages/ProjectGantt.js
@@ -1,6 +1,55 @@
 import Gantt from "../components/Gantt";
 
-const projectTasks = [
+const projects = [
+  {
+    id: 1,
+    proj_name: "KanPlan",
+    manager_id: 1,
+    planned_start: "2021-08-29T18:08:57.766Z",
+    planned_end: "2021-08-29T18:08:57.766Z",
+    proj_description: "Project management(not boring!)",
+    manager_name: "TJ Jung",
+    team_members: [1, 2, 3, 45],
+    project_tasks: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11],
+  },
+  {
+    id: 2,
+    proj_name: "onlyFriends",
+    manager_id: 4,
+    planned_start: "2021-08-29T18:08:57.766Z",
+    planned_end: "2021-08-29T18:08:57.766Z",
+    proj_description:
+      "A tinder-style app to meet new friends with similar interests in your area",
+    manager_name: "Kleir Miranda",
+    team_members: [4, 5, 6, 45],
+    project_tasks: [12, 13, 14, 15],
+  },
+  {
+    id: 3,
+    proj_name: "Dev Community",
+    manager_id: 7,
+    planned_start: "2021-08-29T18:08:57.766Z",
+    planned_end: "2021-08-29T18:08:57.766Z",
+    proj_description:
+      "A social media platform for developers to interact with each other across the globe.",
+    manager_name: "Maggie Zhao",
+    team_members: [7, 8],
+    project_tasks: [16, 17, 18, 19],
+  },
+  {
+    id: 4,
+    proj_name: "Fete",
+    manager_id: 9,
+    planned_start: "2021-08-29T18:08:57.766Z",
+    planned_end: "2021-08-29T18:08:57.766Z",
+    proj_description: "TBD",
+    manager_name: "Sarah Avery",
+    team_members: [9, 10],
+    project_tasks: [null],
+  },
+];
+
+const tasks = [
   {
     id: 1,
     title: "API Routes",
@@ -59,12 +108,17 @@ const projectTasks = [
   },
 ];
 
+const state = {
+  projects,
+  tasks,
+};
+
 const ProjectGantt = () => {
   return (
     <div>
       <h1>This will show the project Gantt.</h1>
       <section className="ganttContainer" style={{ marginLeft: "140px" }}>
-        <Gantt projectTasks={projectTasks} />
+        <Gantt projectTasks={state.tasks} />
       </section>
     </div>
   );
