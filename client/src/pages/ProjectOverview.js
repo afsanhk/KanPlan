400c54c2922239d94fbb68d7cae68a475daca654
Afsanul Haque Khan <afsanhk@gmail.com>
2021-08-30 17:16:07 -0400

    Add header to show when user is on project overview page.

--- client/src/pages/ProjectOverview.js
+++ client/src/pages/ProjectOverview.js
@@ -8,6 +8,7 @@ const ProjectOverview = ({ state, projectID }) => {
   const projectTasks = getTasksForProject(state, projectID).map((i) => state.tasks[i]);
   return (
     <div>
+      <h1>This will show the project overview.</h1>
       <ProjectOverviewTable projectTasks={projectTasks} />
     </div>
   );
