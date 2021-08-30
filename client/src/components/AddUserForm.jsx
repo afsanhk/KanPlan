6acd905c62bedb14a081dc4e0e2d33553595f670
Taejin Jung <taejin5314@gmail.com>
2021-08-30 14:45:46 -0400

    create add user form

---
+++ client/src/components/AddUserForm.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import TeamMember from './TeamMember';
+
+function AddUserForm({ users, userIDs }) {
+  const userIdToName = (userIDs) => {};
+  return (
+    <div className="add-user-form">
+      <TeamMember />
+    </div>
+  );
+}
+
+export default AddUserForm;
