c4c9a37843bdb181d6c2286501ffa34070273bc3
Afsanul Haque Khan <afsanhk@gmail.com>
2021-08-30 18:53:38 -0400

    Add proper props to ProjectList.jsx

--- client/src/components/ProjectList.jsx
+++ client/src/components/ProjectList.jsx
@@ -3,9 +3,14 @@ import List from '@material-ui/core/List';
 
 export default function ProjectList ({projects}) {
   
-  const parsedProjectList = projects.map(el => {
+  const parsedProjectList = projects.map(project => {
     return (
     <ProjectListItem 
+      key = {project.id}
+      id = {project.id}
+      name = {project.proj_name}
+      description = {project.proj_description}
+      team_members = {project.team_members}
     />)
   })
   
