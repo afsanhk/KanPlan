dc131e4402336f35b63d8e8f9004bb2dfa600f82
Veronica Leung <tungtungleung233@hotmail.com>
2021-08-31 10:24:54 -0400

    change Gantt icon

--- client/src/components/LinkIconContainer.jsx
+++ client/src/components/LinkIconContainer.jsx
@@ -1,24 +1,39 @@
+import React, { useEffect } from 'react';
 import Button from '@material-ui/core/Button';
 import DashboardIcon from '@material-ui/icons/Dashboard'; // Project Overview
 import AssessmentIcon from '@material-ui/icons/Assessment'; // Project Gantt
 import ListAltIcon from '@material-ui/icons/ListAlt'; // Project Kanban
 import PeopleIcon from '@material-ui/icons/People';
 import { makeStyles } from '@material-ui/core/styles'; //use this to customize the style
+import Icon from '@material-ui/core/Icon';
+import { loadCSS } from 'fg-loadcss';
 
 import '../styles/LinkIconContainer.scss'
 
+const useStyles = makeStyles(() => ({
+  button: {
+    'margin-right': '5px',
+    color: '#093170',
+    height: '40px',
+    padding: '10px 8px',
+    'text-transform': 'none',
+    'font-size': '16px',
+  },
+}));
+
 export default function LinkIconContainer ({project}) {
   
-  const useStyles = makeStyles(() => ({
-    button: {
-      'margin-right': '5px',
-      color: '#093170',
-      height: '40px',
-      padding: '10px 8px',
-      'text-transform': 'none',
-      'font-size': '16px',
-    },
-  }));
+
+  React.useEffect(() => {
+    const node = loadCSS(
+      'https://use.fontawesome.com/releases/v5.12.0/css/all.css',
+      document.querySelector('#font-awesome-css'),
+    );
+
+    return () => {
+      node.parentNode.removeChild(node);
+    };
+  }, []);
 
   const classes = useStyles();
   
@@ -29,11 +44,11 @@ export default function LinkIconContainer ({project}) {
         <p>Overview</p>
       </Button>
       <Button size="small" className={classes.button} onClick={()=>console.log("Link to project gantt!")}>
-        <AssessmentIcon />
+        <AssessmentIcon className='Gantt-icon'/>
         <p>Gantt</p>
       </Button>
       <Button size="small" className={classes.button} onClick={()=>console.log("Link to project kanban!")}>
-        <ListAltIcon />
+      <Icon className="fab fa-trello" />
         <p>Kanban</p>
       </Button>
       <Button size="small" className={classes.button} onClick={()=>console.log("Pop up users modal!")}>
