2239760ca8307141476ec66b26a2f596e43a45a0
Afsanul Haque Khan <afsanhk@gmail.com>
2021-08-31 17:32:51 -0400

    Add css for AddProjectForm in .scss file in /styles.

--- client/src/components/AddProjectForm.jsx
+++ client/src/components/AddProjectForm.jsx
@@ -1,12 +1,45 @@
 import {useState} from 'react';
+import { TextField } from '@material-ui/core';
+
+import "../styles/AddProjectForm.scss" 
 
 export default function AddProjectForm ({state, userID}) {
 
-  const [name, setName] = useState('');
-  const [desc, setDesc] = useState('');
+  const [projectName, setProjectName] = useState('');
+  const [projectDesc, setProjectDesc] = useState('');
   const [teamMembers, setTeamMembers] = useState([userID])
 
+  const handleSubmit = (event) => {
+    event.preventDefault();
+  }
+
   return (
-    <h1>some kind of form goes here</h1>
+    <>
+      <h1 className='add-form-title'>Add a project</h1>
+      <div className="add-form-body">
+        <form className="add-form" onSubmit={handleSubmit}>
+          <TextField 
+            id="standard-full-width"
+            label="Project Name"
+            style={{ margin: 8 }}
+            placeholder="Write project name here..."
+            multiline
+            margin="normal"
+            onChange={(event) => setProjectName(event.target.value)}
+          />
+
+          <TextField 
+            id="standard-full-width"
+            label="Project Description"
+            style={{ margin: 8 }}
+            placeholder="Write project name here..."
+            multiline
+            margin="normal"
+            onChange={(event) => setProjectDesc(event.target.value)}
+          />
+          
+        </form>
+      </div>
+    </>
   )
 }
