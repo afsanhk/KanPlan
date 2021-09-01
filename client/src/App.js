5c71ca92cc38db657579b1c23acf5be6cb499b4b
Afsanul Haque Khan <afsanhk@gmail.com>
2021-09-01 12:51:14 -0400

    Dumb CSS changes on call

--- client/src/App.js
+++ client/src/App.js
@@ -1,52 +1,52 @@
 // Libraries and frameworks
-import React, { useEffect } from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import React, { useEffect } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
-import useApplicationData from './hooks/useApplicationData';
+import useApplicationData from "./hooks/useApplicationData";
 
 // Components & Pages
-import NavBar from './components/NavBar';
-import UserDashboard from './pages/UserDashboard';
-import UserProjects from './pages/UserProjects';
-import ProjectOverview from './pages/ProjectOverview';
-import ProjectKanban from './pages/ProjectKanban';
-import ProjectGantt from './pages/ProjectGantt';
-import LoadingCircle from './components/LoadingCircle';
+import NavBar from "./components/NavBar";
+import UserDashboard from "./pages/UserDashboard";
+import UserProjects from "./pages/UserProjects";
+import ProjectOverview from "./pages/ProjectOverview";
+import ProjectKanban from "./pages/ProjectKanban";
+import ProjectGantt from "./pages/ProjectGantt";
+import LoadingCircle from "./components/LoadingCircle";
 
 // Styling
-import { createTheme, ThemeProvider } from '@material-ui/core/styles';
-import './styles/App.scss';
+import { createTheme, ThemeProvider } from "@material-ui/core/styles";
+import "./styles/App.scss";
 
 const theme = createTheme({
   palette: {
     primary: {
-      main: '#1e88e5',
-      dark: '#4b9fea' //this color is actually lighter, is visible when hovering over buttons
+      main: "#1e88e5",
+      dark: "#4b9fea", //this color is actually lighter, is visible when hovering over buttons
     },
     secondary: {
-      main: '#e03838',
-      dark: '#ff5252' //this color is actually lighter, is visible when hovering over buttons
-    }
+      main: "#e03838",
+      dark: "#ff5252", //this color is actually lighter, is visible when hovering over buttons
+    },
   },
   typography: {
     fontSize: 16,
     h3: {
-      fontSize: '3.4rem'
-    }
+      fontSize: "3.4rem",
+    },
   },
   overrides: {
     // Style sheet name ⚛️
     MuiAvatar: {
       root: {
-        width: '30px',
-        height: '30px',
-        'font-size': '15px'
+        width: "30px",
+        height: "30px",
+        "font-size": "15px",
       },
       colorDefault: {
-        'background-color': '#1e88e5'
-      }
-    }
-  }
+        "background-color": "#1e88e5",
+      },
+    },
+  },
 });
 
 function App() {
@@ -55,7 +55,7 @@ function App() {
   // Change this projectID to see reflected changes in gantt based on state.
   // In reality we will need to pass this in based on which project we are routing from
   const projectID = 1;
-  const userID = 1;
+  const userID = 2;
 
   return (
     <ThemeProvider theme={theme}>
@@ -74,7 +74,7 @@ function App() {
                   <UserProjects state={state} userID={userID} />
                 </Route>
                 <Route path="/project/:projectID/overview">
-                  <ProjectOverview state={state} deleteTask={deleteTask}/>
+                  <ProjectOverview state={state} deleteTask={deleteTask} />
                 </Route>
                 <Route path="/project/:projectID/kanban">
                   <ProjectKanban state={state} />
