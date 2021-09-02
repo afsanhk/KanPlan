532de9c3a931d20f8af97d900315f2e77b637f7a
Afsanul Haque Khan <afsanhk@gmail.com>
2021-09-02 15:26:25 -0400

    Add some styling to gantt

--- client/src/components/Gantt.jsx
+++ client/src/components/Gantt.jsx
@@ -4,27 +4,31 @@ import '../styles/Gantt.scss'
 
 
 const styleConfig = {
-  header:{ //Targert the time header containing the information month/day of the week, day and time.
-      top:{//Tartget the month elements
+  header:{ //Target the time header containing the information month/day of the week, day and time.
+      top:{//Target the month elements
           style:{backgroundColor:"#333333"} //The style applied to the month elements
       },
-      middle:{//Tartget elements displaying the day of week info
+      middle:{//Target elements displaying the day of week info
           style:{backgroundColor:"chocolate"}, //The style applied to the day of week elements
           selectedStyle:{backgroundColor:"#b13525"}//The style applied to the day of week elements when is selected
       },
-      bottom:{//Tartget elements displaying the day number or time 
+      bottom:{//Target elements displaying the day number or time 
           style:{background:"grey",fontSize:9},//the style tp be applied 
           selectedStyle:{backgroundColor:"#b13525",fontWeight:  'bold'}//the style tp be applied  when selected
       }
   },
   taskList:{//the right side task list
       title:{//The title od the task list
-          label:"Projects",//The caption to display as title
+          label:"Tasks",//The caption to display as title
           style:{backgroundColor:  '#333333',borderBottom:  'solid 1px silver',
                  color:  'white',textAlign:  'center'}//The style to be applied to the title
       },
       task:{// The items inside the list diplaying the task
-          style:{backgroundColor:  '#fbf9f9'}// the style to be applied
+          style:{ // the style to be applied
+            backgroundColor:  '#fbf9f9',
+            textAlign: "left",
+            padding: "5px",
+          }
       },
       verticalSeparator:{//the vertical seperator use to resize he width of the task list
           style:{backgroundColor:  '#333333',},//the style
@@ -34,7 +38,7 @@ const styleConfig = {
       }
   },
   dataViewPort:{//The are where we display the task
-      rows:{//the row constainting a task
+    rows:{//the row constainting a task
           style:{backgroundColor:"#fbf9f9",borderBottom:'solid 0.5px #cfcfcd'}
           },
       task:{//the task itself
@@ -43,10 +47,6 @@ const styleConfig = {
                  textAlign:'center',backgroundColor:'grey'},
            selectedStyle:{}//the style tp be applied  when selected
       }
-  },
-  links:{//The link between two task
-      color:'black',
-      selectedColor:'#ff00fa'
   }
 }
 
@@ -62,16 +62,25 @@ function Gantt({projectTasks}) {
       name: el.title,
       start: el.plan_start,
       end: endDate + 100000000, // Hard coded --> Remove when seeds are improved
-      color: 'blue'
+      color: 'lightblue'
     }
   })
   ;
   
+  // const onUpdateTask = (item, props) => {
+  //   item.start = props.start;
+  //   item.end = props.end;
+  //   this.setState({ data: [...this.state.data] });
+  // };
 
   return (
     <>
       {!projectTasks[0] ? <h1>Ha-ha no data for a Gantt!</h1> :
-      <TimeLine data={tasks} config={styleConfig} />}
+      <TimeLine 
+      data={tasks} 
+      config={styleConfig} 
+      // onUpdateTask={onUpdateTask}
+      />}
     </>
   )
 }
