c198cd0cab1db2377131e515c73c3fbf365c8b75
Afsanul Haque Khan <afsanhk@gmail.com>
2021-09-02 11:51:01 -0400

    Forgot to save after fixing conflict

--- client/src/pages/ProjectKanban.js
+++ client/src/pages/ProjectKanban.js
@@ -22,23 +22,6 @@ const ProjectKanban = ({ state, addTask, updateTaskStatus, getKanbanStatus, kanb
     console.log(state.users[1]);
   }, [projectID, state.projects[projectID].project_tasks]);
 
-<<<<<<< HEAD
-  const moveInArray = function (arr, from, to) {
-    if (Object.prototype.toString.call(arr) !== "[object Array]") {
-      throw new Error("Please provide a valid array");
-    }
-
-    const item = arr.splice(from, 1);
-
-    if (!item.length) {
-      throw new Error("There is no item in the array at index" + from);
-    }
-
-    arr.splice(to, 0, item[0]);
-  };
-
-=======
->>>>>>> master
   const initialData = {
     tasks: {},
     columns: {
