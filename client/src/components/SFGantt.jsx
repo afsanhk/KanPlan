a95ae1d4c6ddf74d2a34099fe92bddc1d66e33fa
Afsanul Haque Khan <afsanhk@gmail.com>
2021-09-03 17:08:38 -0400

    Add toggle button to show or hide dates

--- client/src/components/SFGantt.jsx
+++ client/src/components/SFGantt.jsx
@@ -1,8 +1,11 @@
-import { ColumnDirective, ColumnsDirective, GanttComponent } from '@syncfusion/ej2-react-gantt';
+import {useState, useCallback} from 'react';
 
+import { ColumnDirective, ColumnsDirective, GanttComponent } from '@syncfusion/ej2-react-gantt';
 import "../styles/Gantt.scss"
 
 function SFGantt({ projectTasks }) {
+  const [showDates, setShowDates] = useState(true);
+  const toggle = useCallback(() => setShowDates(!showDates))
   const taskValues = {
     id: 'id',
     name: 'name',
@@ -24,11 +27,12 @@ function SFGantt({ projectTasks }) {
 
   return (
     <div>
+      <button onClick={toggle}>{showDates?'Hide Dates':'Show Dates'}</button>
       <GanttComponent dataSource={tasks} taskFields={taskValues}>
         <ColumnsDirective>
           <ColumnDirective field="name" headerText="Task Names" headerTextAlign="Center"></ColumnDirective>
-          {/* <ColumnDirective field="start_date" format="dd-MMM-yy"></ColumnDirective>
-          <ColumnDirective field="end_date" format="dd-MMM-yy"></ColumnDirective> */}
+          {showDates && <ColumnDirective field="start_date" format="dd-MMM-yy"></ColumnDirective>}
+          {showDates && <ColumnDirective field="end_date" format="dd-MMM-yy"></ColumnDirective>}
         </ColumnsDirective>
       </GanttComponent>
     </div>
