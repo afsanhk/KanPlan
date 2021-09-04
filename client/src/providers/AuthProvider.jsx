923c745f7c18a29628ab70a6324f40c97033bc7d
Afsanul Haque Khan <afsanhk@gmail.com>
2021-09-04 17:03:22 -0400

    Initialize AuthProvider.jsx using Gary's lecture notes.

---
+++ client/src/providers/AuthProvider.jsx
@@ -0,0 +1,29 @@
+import { createContext, useState } from 'react';
+
+export default function AuthProvider(props) {
+  const [auth, setAuth] = useState(false);
+  const [user, setUser] = useState({ email: "", name: "", id: "" });
+
+  // Perform login process for the user & save authID, etc
+  const login = function (email, password) {
+    setUser({ email, id, name: "Test User" });
+    setAuth(true);
+  };
+
+  const logout = function (email, password) {
+    setUser({ email: "", name: "" });
+    setAuth(false);
+  };
+
+  // authContext will expose these items
+  const userData = { auth, user, login, logout };
+
+  // We can use this component to wrap any content we want to share this context
+  return (
+    <authContext.Provider value={userData}>
+      {props.children}
+    </authContext.Provider>
+  );
+};
+
+export const authContext = createContext();
