14f2504750a90c87de64ddd052b5f0b8a5b930f5
Afsanul Haque Khan <afsanhk@gmail.com>
2021-09-05 16:56:32 -0400

    Ensure that useMessages is loading message data and accessible within Chat.js

--- client/src/hooks/useMessages.js
+++ client/src/hooks/useMessages.js
@@ -2,16 +2,15 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 
 export default function useApplicationData() {
-  const [messages, setMessages] = useState({ messages: [] });
+  const [messageHistory, setMessageHistory] = useState([]);
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     axios.get("http://localhost:8001/api/messages").then((res) => {
-      setMessages((prev) => ({
-        ...prev,
-        messages: res[0].data,
-      }));
+      setMessageHistory([...res.data]);
       setLoading(false);
     });
   }, []); //empty square brackets ensures that this useEffect is only ran once during page load
+
+  return { messageHistory };
 }
--- client/src/pages/Chat.js
+++ client/src/pages/Chat.js
@@ -1,7 +1,13 @@
+import useMessages from "../hooks/useMessages";
+
 export default function Chat({ userID }) {
+  const { messageHistory } = useMessages();
+
+  console.log(messageHistory);
   return (
     <>
       <h1>This page will show chat messages!</h1>
+      <p>{messageHistory.map((el) => el.message_text)}</p>
     </>
   );
 }
