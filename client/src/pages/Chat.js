634b56efbd2007803c48f0e6d094007669b2ebc7
Afsanul Haque Khan <afsanhk@gmail.com>
2021-09-05 17:02:07 -0400

    Chat messages are showing

--- client/src/pages/Chat.js
+++ client/src/pages/Chat.js
@@ -4,10 +4,12 @@ export default function Chat({ userID }) {
   const { messageHistory } = useMessages();
 
   console.log(messageHistory);
+  let messageHistoryCopy = JSON.parse(JSON.stringify(messageHistory));
+
   return (
     <>
       <h1>This page will show chat messages!</h1>
-      <p>{messageHistory.map((el) => el.message_text)}</p>
+      <p>{messageHistoryCopy.map((el) => el.message_text)}</p>
     </>
   );
 }
