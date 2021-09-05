c79947470b70b20ab3fc258ec4541d4492783c52
Afsanul Haque Khan <afsanhk@gmail.com>
2021-09-05 16:35:36 -0400

    Add Chat Page to router

--- client/src/App.js
+++ client/src/App.js
@@ -18,6 +18,7 @@ import Login from "./pages/Login";
 // Styling
 import { createTheme, ThemeProvider } from "@material-ui/core/styles";
 import "./styles/App.scss";
+import Chat from "./pages/Chat.js";
 
 const theme = createTheme({
   palette: {
@@ -89,65 +90,68 @@ function App() {
   // In reality we will need to pass this in based on which project we are routing from
   // console.log("Inside App.js userID is:", userID);
 
-  const userID = 1
+  const userID = 1;
   return (
     <ThemeProvider theme={theme}>
       <div className="App">
         {/* {!auth && <Login users={state.users} />}
         {auth && ( */}
-          <Router>
-            <NavBar userID={userID} />
-            {loading ? (
-              <LoadingCircle />
-            ) : (
-              <div className="body">
-                <Switch>
-                  <Route exact path="/">
-                    <UserDashboard state={state} userID={userID} deleteTask={deleteTask} editTask={editTask} />
-                  </Route>
-                  <Route path="/projects">
-                    <UserProjects
-                      state={state}
-                      userID={userID}
-                      addProject={addProject}
-                      deleteProject={deleteProject}
-                      updateProjectUsers={updateProjectUsers}
-                    />
-                  </Route>
-                  <Route path="/project/:projectID/overview">
-                    <ProjectOverview
-                      state={state}
-                      userID={userID}
-                      deleteTask={deleteTask}
-                      deleteProject={deleteProject}
-                      updateProjectUsers={updateProjectUsers}
-                      addTask={addTask}
-                      editTask={editTask}
-                      updateTaskStatus={updateTaskStatus}
-                      updateTaskPriority={updateTaskPriority}
-                      editProject={editProject}
-                    />
-                  </Route>
-                  <Route path="/project/:projectID/kanban">
-                    <ProjectKanban
-                      state={state}
-                      addTask={addTask}
-                      updateKanbanOrder={updateKanbanOrder}
-                      getKanbanStatus={getKanbanStatus}
-                      kanbanStatus={kanbanStatus}
-                      updateProjectUsers={updateProjectUsers}
-                    />
-                  </Route>
-                  <Route path="/project/:projectID/gantt">
-                    <ProjectGantt state={state} updateProjectUsers={updateProjectUsers} />
-                  </Route>
-                  <Route path="*">
-                    <h1>404 - Not Found</h1>
-                  </Route>
-                </Switch>
-              </div>
-            )}
-          </Router>
+        <Router>
+          <NavBar userID={userID} />
+          {loading ? (
+            <LoadingCircle />
+          ) : (
+            <div className="body">
+              <Switch>
+                <Route exact path="/">
+                  <UserDashboard state={state} userID={userID} deleteTask={deleteTask} editTask={editTask} />
+                </Route>
+                <Route path="/projects">
+                  <UserProjects
+                    state={state}
+                    userID={userID}
+                    addProject={addProject}
+                    deleteProject={deleteProject}
+                    updateProjectUsers={updateProjectUsers}
+                  />
+                </Route>
+                <Route path="/project/:projectID/overview">
+                  <ProjectOverview
+                    state={state}
+                    userID={userID}
+                    deleteTask={deleteTask}
+                    deleteProject={deleteProject}
+                    updateProjectUsers={updateProjectUsers}
+                    addTask={addTask}
+                    editTask={editTask}
+                    updateTaskStatus={updateTaskStatus}
+                    updateTaskPriority={updateTaskPriority}
+                    editProject={editProject}
+                  />
+                </Route>
+                <Route path="/project/:projectID/kanban">
+                  <ProjectKanban
+                    state={state}
+                    addTask={addTask}
+                    updateKanbanOrder={updateKanbanOrder}
+                    getKanbanStatus={getKanbanStatus}
+                    kanbanStatus={kanbanStatus}
+                    updateProjectUsers={updateProjectUsers}
+                  />
+                </Route>
+                <Route path="/project/:projectID/gantt">
+                  <ProjectGantt state={state} updateProjectUsers={updateProjectUsers} />
+                </Route>
+                <Route path="/chat">
+                  <Chat userID={userID} />
+                </Route>
+                <Route path="*">
+                  <h1>404 - Not Found</h1>
+                </Route>
+              </Switch>
+            </div>
+          )}
+        </Router>
         {/* )} */}
       </div>
     </ThemeProvider>
