8928c02a0fde53a33cf302df8668a818ad600155
Afsanul Haque Khan <afsanhk@gmail.com>
2021-09-05 18:01:24 -0400

    Set up initial server-side code for socket connection. Next step is to attempt connection from client side

--- server/src/index.js
+++ server/src/index.js
@@ -1,35 +1,18 @@
 const PORT = process.env.PORT || 8001;
-const ENV = require('./environment');
+const ENV = require("./environment");
 
-const app = require('./application')(ENV);
-const server = require('http').Server(app);
+const app = require("./application")(ENV);
+const socketio = require("socket.io");
+const server = require("http").Server(app);
 
-const WebSocket = require('ws');
-const wss = new WebSocket.Server({ server });
-
-wss.on('connection', (socket) => {
-  socket.onmessage = (event) => {
-    console.log(`Message Received: ${event.data}`);
-
-    if (event.data === 'ping') {
-      socket.send(JSON.stringify('pong'));
-    }
-  };
+// Socket IO Connections
+const io = socketio(server, {
+  cors: { origin: "http://localhost:8000", methods: ["GET", "POST", "PUT", "DELETE"] },
 });
 
-// function updateAppointment(id, interview) {
-//   wss.clients.forEach(function eachClient(client) {
-//     if (client.readyState === WebSocket.OPEN) {
-//       client.send(
-//         JSON.stringify({
-//           type: "SET_INTERVIEW",
-//           id,
-//           interview
-//         })
-//       );
-//     }
-//   });
-// }
+io.on("connection", (socket) => {
+  console.log("We have a new connection!", socket.id);
+});
 
 server.listen(PORT, () => {
   console.log(`Listening on port ${PORT} in ${ENV} mode.`);
