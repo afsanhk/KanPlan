69ac0c41390b4f8fcdb98e09d74cbe77e0f7fd64
Afsanul Haque Khan <afsanhk@gmail.com>
2021-09-05 17:20:17 -0400

    Set up ChatMessageList component. Next step is to use in Chat.js

--- client/src/components/ChatMessageList.jsx
+++ client/src/components/ChatMessageList.jsx
@@ -3,12 +3,12 @@ import ScrollToBottom from "react-scroll-to-bottom";
 import ChatMessage from "./ChatMessage";
 
 // We use react-scroll-to-bottom here. It auto scrolls to bottom when height of messages exceeds the height of the container.
-const ChatMessageList = ({ messages, name }) => {
+const ChatMessageList = ({ messages, userID, users }) => {
   return (
     <ScrollToBottom className="messages">
-      {messages.map((message, i) => (
-        <div key={i}>
-          <ChatMessage message={message} name={name} />
+      {messages.map((message) => (
+        <div key={messages.id}>
+          <ChatMessage message={message} userID={userID} users={users}/>
         </div>
       ))}
     </ScrollToBottom>
