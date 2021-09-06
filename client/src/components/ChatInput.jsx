263a475c3474733c1ed4cf0db23a46ab499f5289
Afsanul Haque Khan <afsanhk@gmail.com>
2021-09-05 22:19:53 -0400

    Replace ChatInput form with material UI components.

--- client/src/components/ChatInput.jsx
+++ client/src/components/ChatInput.jsx
@@ -1,22 +1,25 @@
 import React from "react";
 
+import {TextField, Button} from '@material-ui/core';
+
 const ChatInput = ({ message, setMessage, sendMessage }) => {
   return (
-    <form className="chat-input">
-      <input
-        className="input"
-        type="text"
-        value={message}
-        onChange={(event) => setMessage(event.target.value)}
-        // Pressing enter sends the chat message
-        onKeyPress={(event) =>
-          event.key === "Enter" ? sendMessage(event) : null
-        }
-      />
-      <button className="send-button" onClick={(event) => sendMessage(event)}>
+    <div className="chat-input">
+      <TextField
+          id="outlined-multiline-static"
+          multiline
+          variant="outlined"
+          value={message}
+          onChange={(event) => setMessage(event.target.value)}
+          // Pressing enter sends the message
+          onKeyPress={(event) =>
+            event.key === "Enter" ? sendMessage(event) : null
+          }
+        />
+      <Button className="send-button" onClick={(event) => sendMessage(event)}>
         Send
-      </button>
-    </form>
+      </Button>
+    </div>
   );
 };
 
--- client/src/components/ChatMessage.jsx
+++ client/src/components/ChatMessage.jsx
@@ -7,20 +7,20 @@ export default function ChatMessage({message, userID, users}){
   }
 
   return loggedIn ? (
-    <div className="message-container logged-in justifyEnd">
-      <p className="sent-text pr-10">{users[message.user_id].user_name}</p>
-      <div className="message-box">
+    <div className="message-container ">
+      <div className="message-box logged-in">
+        <p className="sent-text">{users[message.user_id].user_name}</p>
         <p className="message-text">{message.message_text}</p>
         <p className="message-time">{(message.created_at)}</p>
       </div>
     </div>
   ) : (
-    <div className="message-container not-logged-in justifyStart">
-      <div className="message-box backgroundLight">
-        <p className="message-text colorDark">{message.message_text}</p>
+    <div className="message-container">
+      <div className="message-box">
+        <p className="sent-text">{users[message.user_id].user_name}</p>
+        <p className="message-text">{message.message_text}</p>
+        <p className="message-time">{(message.created_at)}</p>
       </div>
-      <p className="sent-text pl-10">{users[message.user_id].user_name}</p>
-      <p className="message-time">{(message.created_at)}</p>
     </div>
   )
 } 
