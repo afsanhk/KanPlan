fab52e211642a4fbe911ac723f4ff7ddf69b8eb7
Afsanul Haque Khan <afsanhk@gmail.com>
2021-09-05 23:37:44 -0400

    Install react-moment and moment @2.29.0, refactor code to use variables in ChatMessage.jsx

--- client/src/components/ChatMessage.jsx
+++ client/src/components/ChatMessage.jsx
@@ -1,3 +1,5 @@
+import Moment from 'react-moment';
+
 export default function ChatMessage({message, userID, users}){
 
   let loggedIn = false;
@@ -6,24 +8,28 @@ export default function ChatMessage({message, userID, users}){
     loggedIn = true;
   }
 
+  const username = users[message.user_id].user_name;
+  const time = <Moment format="YYYY/MM/DD hh:mm a">{message.created_at}</Moment>
+  const messageContent = message.message_text;
+
   return loggedIn ? (
     <div className="message-container ">
       <div className="message-box logged-in">
         <div className="sent-text-header">
-          <p className="message-sender">{users[message.user_id].user_name}</p>
-          <p className="message-time">{(message.created_at)}</p>
+          <p className="message-sender">{username}</p>
+          <p className="message-time">{time}</p>
         </div>
-        <p className="message-text">{message.message_text}</p>
+        <p className="message-text">{messageContent}</p>
       </div>
     </div>
   ) : (
     <div className="message-container">
       <div className="message-box">
       <div className="sent-text-header">
-        <p className="message-time">{message.created_at}</p>
-        <p className="message-sender">{users[message.user_id].user_name}</p>
+        <p className="message-time">{time}</p>
+        <p className="message-sender">{username}</p>
       </div>
-      <p className="message-text">{message.message_text}</p>  
+      <p className="message-text">{messageContent}</p>  
       </div>
     </div>
   )
