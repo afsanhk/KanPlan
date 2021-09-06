004bd1f0e99bcf2c2886e032d209c65da9d5ba5f
Afsanul Haque Khan <afsanhk@gmail.com>
2021-09-05 21:35:01 -0400

    Confirm chat functionality is 100% done. Remove auth code for easier code testing. Next step is styling for auth and chat.

--- client/src/App.js
+++ client/src/App.js
@@ -85,74 +85,74 @@ function App() {
     editProject,
   } = useApplicationData();
 
-  const { auth, userID } = useContext(authContext);
+  // const { auth, userID } = useContext(authContext);
   // Change this projectID to see reflected changes in gantt based on state.
   // In reality we will need to pass this in based on which project we are routing from
   // console.log("Inside App.js userID is:", userID);
 
-  // const userID = 1;
+  const userID = 1;
   return (
     <ThemeProvider theme={theme}>
       <div className="App">
-        {!auth && <Login users={state.users} />}
-        {auth && (
-          <Router>
-            <NavBar userID={userID} />
-            {loading ? (
-              <LoadingCircle />
-            ) : (
-              <div className="body">
-                <Switch>
-                  <Route exact path="/">
-                    <UserDashboard state={state} userID={userID} deleteTask={deleteTask} editTask={editTask} />
-                  </Route>
-                  <Route path="/projects">
-                    <UserProjects
-                      state={state}
-                      userID={userID}
-                      addProject={addProject}
-                      deleteProject={deleteProject}
-                      updateProjectUsers={updateProjectUsers}
-                    />
-                  </Route>
-                  <Route path="/project/:projectID/overview">
-                    <ProjectOverview
-                      state={state}
-                      userID={userID}
-                      deleteTask={deleteTask}
-                      deleteProject={deleteProject}
-                      updateProjectUsers={updateProjectUsers}
-                      addTask={addTask}
-                      editTask={editTask}
-                      updateTaskStatus={updateTaskStatus}
-                      updateTaskPriority={updateTaskPriority}
-                      editProject={editProject}
-                    />
-                  </Route>
-                  <Route path="/project/:projectID/kanban">
-                    <ProjectKanban
-                      state={state}
-                      addTask={addTask}
-                      updateKanbanOrder={updateKanbanOrder}
-                      getKanbanStatus={getKanbanStatus}
-                      kanbanStatus={kanbanStatus}
-                      updateProjectUsers={updateProjectUsers}
-                    />
-                  </Route>
-                  <Route path="/project/:projectID/gantt">
-                    <ProjectGantt state={state} updateProjectUsers={updateProjectUsers} />
-                  </Route>
-                  <Route path="/chat">
-                    <Chat userID={userID} users={state.users} />
-                  </Route>
-                  <Route path="*">
-                    <h1>404 - Not Found</h1>
-                  </Route>
-                </Switch>
-              </div>
-            )}
-          </Router>
-        )}
+        {/* {!auth && <Login users={state.users} />} */}
+        {/* {auth && ( */}
+        <Router>
+          <NavBar userID={userID} />
+          {loading ? (
+            <LoadingCircle />
+          ) : (
+            <div className="body">
+              <Switch>
+                <Route exact path="/">
+                  <UserDashboard state={state} userID={userID} deleteTask={deleteTask} editTask={editTask} />
+                </Route>
+                <Route path="/projects">
+                  <UserProjects
+                    state={state}
+                    userID={userID}
+                    addProject={addProject}
+                    deleteProject={deleteProject}
+                    updateProjectUsers={updateProjectUsers}
+                  />
+                </Route>
+                <Route path="/project/:projectID/overview">
+                  <ProjectOverview
+                    state={state}
+                    userID={userID}
+                    deleteTask={deleteTask}
+                    deleteProject={deleteProject}
+                    updateProjectUsers={updateProjectUsers}
+                    addTask={addTask}
+                    editTask={editTask}
+                    updateTaskStatus={updateTaskStatus}
+                    updateTaskPriority={updateTaskPriority}
+                    editProject={editProject}
+                  />
+                </Route>
+                <Route path="/project/:projectID/kanban">
+                  <ProjectKanban
+                    state={state}
+                    addTask={addTask}
+                    updateKanbanOrder={updateKanbanOrder}
+                    getKanbanStatus={getKanbanStatus}
+                    kanbanStatus={kanbanStatus}
+                    updateProjectUsers={updateProjectUsers}
+                  />
+                </Route>
+                <Route path="/project/:projectID/gantt">
+                  <ProjectGantt state={state} updateProjectUsers={updateProjectUsers} />
+                </Route>
+                <Route path="/chat">
+                  <Chat userID={userID} users={state.users} />
+                </Route>
+                <Route path="*">
+                  <h1>404 - Not Found</h1>
+                </Route>
+              </Switch>
+            </div>
+          )}
+        </Router>
+        {/* )} */}
       </div>
     </ThemeProvider>
   );
