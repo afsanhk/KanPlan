0ba4fd521fe6b10b1512a15199abd2067608bb28
Afsanul Haque Khan <afsanhk@gmail.com>
2021-09-07 12:31:07 -0400

    Change countdown message font color for webcam picture

--- client/src/utils/drawFaceContainer.js
+++ client/src/utils/drawFaceContainer.js
@@ -10,16 +10,18 @@ const drawFaceContainer = (ctx, detections, canvasWidth, canvasHeight, count, co
     const leftEar = [landmarks[5][0], landmarks[5][1]];
 
     if (count === countDown) {
-      ctx.font = '30px Arial';
-      ctx.fillText('Face Detected!', canvasWidth - 230, 50);
+      ctx.font = "30px Arial";
+      ctx.fillStyle = "white";
+      ctx.fillText("Face Detected!", canvasWidth - 230, 50);
     } else if (count > countDown && count <= countDown + 4) {
-      ctx.font = '30px Arial';
-      ctx.fillText(`CountDown: ${countDown + 4 - count}`, canvasWidth - 230, 50);
+      ctx.font = "30px Arial";
+      ctx.fillStyle = "white";
+      ctx.fillText(`Countdown: ${countDown + 4 - count}`, canvasWidth - 230, 50);
     } else if (count < countDown) {
       ctx.scale(-1, 1);
-      ctx.fillStyle = 'rgba(256,0,0,0.3)';
+      ctx.fillStyle = "rgba(256,0,0,0.3)";
       ctx.fillRect(topLeft[0] - canvasWidth, topLeft[1], bottomRight[0] - topLeft[0], bottomRight[1] - topLeft[1]);
-      ctx.fillStyle = 'black';
+      ctx.fillStyle = "black";
       ctx.fillRect(rightEye[0] - canvasWidth, rightEye[1], 5, 5);
       ctx.fillRect(leftEye[0] - canvasWidth, leftEye[1], 5, 5);
       ctx.fillRect(nose[0] - canvasWidth, nose[1], 5, 5);
@@ -27,8 +29,9 @@ const drawFaceContainer = (ctx, detections, canvasWidth, canvasHeight, count, co
       ctx.fillRect(rightEar[0] - canvasWidth, rightEar[1], 5, 5);
       ctx.fillRect(leftEar[0] - canvasWidth, leftEar[1], 5, 5);
     } else if (count === countDown + 5) {
-      ctx.font = '30px Arial';
-      ctx.fillText('Taking Picture!', canvasWidth - 230, 50);
+      ctx.font = "30px Arial";
+      ctx.fillStyle = "white";
+      ctx.fillText("Taking Picture!", canvasWidth - 230, 50);
     } else {
       ctx.clearRect(0, 0, canvasWidth, canvasHeight);
     }
